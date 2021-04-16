ed10eb99d87920f135f28ee117ef85d95c87743e
Francesca Marino <fjmarino90@gmail.com>
2021-04-16 14:36:10 +0900

    updated connection variables

--- knexfile.js
+++ knexfile.js
@@ -12,9 +12,9 @@ module.exports = {
     client: "pg",
     connection: {
       host: "127.0.0.1",
-      user: process.env.DB_USER,
-      password: process.env.DB_PW,
-      database: process.env.DB_NAME,
+      user: process.env.DB_DEV_USER,
+      password: process.env.DB_DEV_PW,
+      database: process.env.DB_DEV_NAME,
     },
     migrations: {
       directory: __dirname + "/db/migrations",
