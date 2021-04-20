44a67e4463e3ee809cbdb5ee91a8efe0f9d62c55
Francesca Marino <fjmarino90@gmail.com>
2021-04-20 10:44:23 +0900

    updating tables

---
+++ db/migrations/20210420013524_create_photo_table.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("photos", function (table) {
+    table.increments("id").primary();
+    table.string("url").notNullable();
+    table.string("flightID").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("photos");
+};
